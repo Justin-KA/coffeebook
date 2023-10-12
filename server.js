8747589e7c85c3afd8dc9966a156c1d0a1038e9a
Justin KA <justinalexander101@gmail.com>
2023-10-12 19:23:07 -0400

    middleware for passport and session

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express()
 const mongoose = require('mongoose')
 const connectDB = require('./config/connectDB')
 const coffeeRoutes = require('./routes/coffeeRoutes')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
+const User = require('./models/userModel')
+const session = require('express-session')
+
 const PORT = process.env.PORT || 3500
 
 connectDB()
@@ -12,8 +17,26 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 app.use(express.static('public'))
-
 app.set('view engine', 'ejs')
+
+app.use(session({
+    secret: 'this is CoffeeBook',
+    resave: false,
+    saveUninitialized: false
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+//passing current user info to all routes
+app.use((req, res, next) => {
+    res.locals.currentUser = req.user
+    next()
+})
 
 app.use('/', coffeeRoutes)
 
